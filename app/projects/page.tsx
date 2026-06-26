acdc0fd2dfaf55a7087c3d1df897c60eba2dba58
VictorJohnson <okikevictorodinaka@gmail.com>
2026-06-26 15:42:43 +0100

    refactor: remove NatureSection component from ProjectsPage

--- app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import NatureSection from "../components/NatureSection";
-
-export default function ProjectsPage() {
-  return (
-    <main className="pt-24">
-      <NatureSection />
-    </main>
-  );
-}
